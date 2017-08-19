4daa0effd9e9ef0877a717737f7c7b442a23955e
Dotun Longe <oludotunlonge@gmail.com>
2017-08-19 21:02:42 +0100

    extended Array Constructor with a new method
    new method takes an argument to determine the order i.e ascending or descending of the values contained within the array.

--- sort.js
+++ sort.js
@@ -11,3 +11,31 @@ function sort(array) {
     }
     return arr;
 }
+
+Array.prototype.sort = (order) => {
+    if (order == "down") {
+        for (let i = 0; i < arr.length; i++) {
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] < arr[i + 1]) {
+                    let one = arr[i];
+                    let two = arr[i + 1];
+                    arr[i] = two;
+                    arr[i + 1] = one;
+                }
+            }
+        }
+    } else if (order == "up"){
+        for (let i = 0; i < arr.length; i++) {
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    let one = arr[i];
+                    let two = arr[i + 1];
+                    arr[i] = two;
+                    arr[i + 1] = one;
+                }
+            }
+        }
+    }
+    return arr;
+
+}
